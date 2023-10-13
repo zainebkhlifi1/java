e3a857fe469cd91fadaabbe4cc969f286ec3ab6e
zainebkhlifi1 <zaineb.khlifi@esprit.tn>
2023-10-13 16:11:32 +0100

    first commit

--- src/Exception/DivisionExecption.java
+++ src/Exception/DivisionExecption.java
@@ -19,11 +19,13 @@ public class DivisionExecption {
      static  int x =20;
      static int y ;
           public static void main(String[] args) {
-         Scanner scanner = new Scanner(System.in);
-            System.out.println(" Enter un entier");
-             y = scanner.nextInt();
-            System.out.println(x/y);
-    }
+              try {
+                  Scanner scanner = new Scanner(System.in);
+                  System.out.println(" Enter un entier");
+                  y = scanner.nextInt();
+                  System.out.println(x / y);
+              } catch (ArithmeticException AE) {
+                  System.out.println("Arithmetic Exception");
 
-
-}
+              }
+          }}
--- src/Exception/SuperHotel.java
+++ src/Exception/SuperHotel.java
@@ -13,15 +13,19 @@ public class SuperHotel {
     static int x[];
 
 
-    public void reserver(){
-    
-              x[0] = 1;     
+    public void reserver() {
+
+        x[0] = 1;
     }
-    
+
     public static void main(String args[]) {
-        
-        SuperHotel s = new SuperHotel();
-        s.reserver();
+        try {
+
+            SuperHotel s = new SuperHotel();
+            s.reserver();
+        }
+        catch (NullPointerException Null){
+            System.out.println("hhhhhhh");
+        }
     }
 }
-
